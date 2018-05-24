5f21a021824611953d0ec0f735b56fd7a97e915e
Triqueneaux Calvin <calvin8313@hotmail.fr>
2018-05-24 20:12:01 +0200

    bug background image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  RAILS_ENV=production rake assets:precompile
+  config.serve_static_assets = true
+  config.assets.compile = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -27,9 +28,6 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
