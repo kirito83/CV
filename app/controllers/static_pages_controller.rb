ee46747c137b5e537b0e75cd9efd7069eeccc51c
Triqueneaux Calvin <calvin8313@hotmail.fr>
2018-06-05 06:09:24 +0200

    ajout page biographie

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def bio
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'accueil', to: 'static_pages#accueil'
   get 'contact', to: 'static_pages#contact'
+  get 'bio', to: 'static_pages#bio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :contacts, only: [:new, :create]
   root 'static_pages#accueil'
